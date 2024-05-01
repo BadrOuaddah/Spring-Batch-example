48cc6bfe9870feb438c05fd82bd24a18fef4343c
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2024-05-01 15:46:07 +0100

    Create Job and BatchProcessingApplication

---
+++ src/main/java/com/example/springBatchExample/JobCompletionNotificationListener.java
@@ -0,0 +1,19 @@
+package com.example.springBatchExample;
+
+import org.springframework.batch.core.JobExecutionListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+
+
+public class JobCompletionNotificationListener implements JobExecutionListener {
+    private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
+
+    @Override
+    public void afterJob(JobExecution jobExecution) {
+        if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
+            log.info("JOB FINISHED !!");
+        }
+    }
+}
