7ed2962d21ae2a546c127df02bc8f50e0a1f9e59
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2024-05-02 10:54:38 +0100

    Add @EnableBatchProcessing annotation

--- src/main/java/com/example/springBatchExample/BatchProcessingApplication.java
+++ src/main/java/com/example/springBatchExample/BatchProcessingApplication.java
@@ -3,13 +3,16 @@ package com.example.springBatchExample;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+// TODO: Parameter 2 of method step1 in com.example.springBatchExample.CsvToDatabaseJob required a bean of type 'org.springframework.batch.item.ItemProcessor' that could not be found.
 @SpringBootApplication
+@EnableBatchProcessing
 public class BatchProcessingApplication implements CommandLineRunner {
 
     private final JobLauncher jobLauncher;
