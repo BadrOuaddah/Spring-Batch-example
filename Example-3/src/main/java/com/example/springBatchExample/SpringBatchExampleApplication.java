e08d956b4410a1140d488181853a2c36ab81439d
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2024-06-04 00:36:57 +0100

    Linting code

--- Example-3/src/main/java/com/example/springBatchExample/SpringBatchExampleApplication.java
+++ Example-3/src/main/java/com/example/springBatchExample/SpringBatchExampleApplication.java
@@ -8,8 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableBatchProcessing
 public class SpringBatchExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBatchExampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchExampleApplication.class, args);
+    }
 
 }
--- Example-3/src/main/java/com/example/springBatchExample/entity/Gender.java
+++ Example-3/src/main/java/com/example/springBatchExample/entity/Gender.java
@@ -1,5 +1,5 @@
 package com.example.springBatchExample.entity;
 
 public enum Gender {
-    Male,Female
+    Male, Female
 }
